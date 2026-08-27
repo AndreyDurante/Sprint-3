c4d12ae7a16f92a88a387edce42d36a2734829fe
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-26 21:54:02 -0300

    Criando a seção de público alvo

--- sprint/src/App.jsx
+++ sprint/src/App.jsx
@@ -1,24 +1,42 @@
 import './App.css'
 import Botao from './components/Botao'
 import Head from './components/Head'
+import Solucao from './components/Solucao'
 
 function App() {
   return(
+    <div>
       <section className='landing'>
-        <Head></Head>
-          <article className='conteudo-landing'>
-            <h6>Lorem ipsum dolor sit amet.</h6>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus explicabo, debitis fugit error rerum consequatur.</p>
-            <Botao
-            texto = "Conheça a solução"
-            
-            />
-          </article>
-          <div className='imagem-landing'>
+          <Head></Head>
+            <article className='conteudo-landing'>
+              <h6>Lorem ipsum dolor sit amet.</h6>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus explicabo, debitis fugit error rerum consequatur.</p>
+              <Botao
+              texto = "Conheça a solução"
+              
+              />
+            </article>
+            <div className='imagem-landing'>
 
+            </div>
+        </section>
+
+        <section className='solucao'>
+          <h6>Título solução</h6>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus explicabo, debitis fugit error rerum consequatur.</p> 
+          <div className='cards-solucao'>
+            <Solucao/>
+            <Solucao/>
+            <Solucao/>
+            <Solucao/>
           </div>
-      </section>
+        </section>
 
+        <section className='publico-alvo'>
+          <h2>Público Alvo</h2>
+          
+        </section>
+    </div>
       
     
   )
---
+++ sprint/src/components/Publico.jsx
@@ -0,0 +1,9 @@
+import "./publico.css"
+
+const Publico = () => {
+    return (
+        <div className="card-publico">
+            <div className="imagem-publico"></div>
+        </div>
+    )
+}
