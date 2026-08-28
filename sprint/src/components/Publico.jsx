f2b452eae897a90a0f13bc931244bb8bb54112f3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-27 21:07:02 -0300

    Estilização1

--- sprint/src/components/Publico.jsx
+++ sprint/src/components/Publico.jsx
@@ -2,10 +2,10 @@ import "./publico.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
-const Publico = ({titulo, descricao}) => {
+const Publico = ({ titulo, descricao }) => {
     return (
         <div className="card-publico">
-            <FontAwesomeIcon icon={faCircleUser} className="icone" />
+            <FontAwesomeIcon icon={faCircleUser} className="icone-publico" />
             <div className="imagem-publico"></div>
             <h6 className="nome">{titulo}</h6>
             <p className="descricao-publico">{descricao}</p>
